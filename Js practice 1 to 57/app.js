7dfaffb5cd4f3b5158d16cf4c622620ddf4cc067
Syed ADil <101401945+syedadils@users.noreply.github.com>
2022-08-13 05:37:23 -0700

    Add files via upload

---
+++ Js practice 1 to 57/app.js
@@ -0,0 +1,166 @@
+                                             // chap # 1 alert
+// alert("Thanks for your input!");
+// alert ( "Thanks for your input!" );
+
+                                         // chap # 2 Variables for Strings
+// var name = "Mark";
+// alert(name)
+// name = "Ace"; 
+// alert(name);
+// var nationality;
+// nationality = "U.S."; 
+// alert(nationality);
+// var floogle = "Mark"; 
+// var name = "Mark"; 
+// var lessonAuthor = "Mark";
+// var guyWhoKeepsSayingHisOwnName = "Mark";
+// var x = "Mark"; 
+// var lastName = "Smith"; 
+// var cityOfOrigin = "New Orleans";
+// var aussieGreeting = "g'Day";
+// var thanx = "Thanks for your input!" 
+// alert(thanx); 
+
+                                        // chap # 3 Variables for Numbers 
+// var weight = 150;
+// weight + 25
+// alert(weight);
+// var originalNum = 23; 
+// var newNum = originalNum + 7;
+// alert(newNum);
+
+                                        // chap # 4 Variable Names Legal and Illegal 
+// userResponse 
+// userResponseTime 
+// userResponseTimeLimit 
+// response 
+
+                                       // chap # 5 Math expressions: Familiar operators 
+// var popularNumber = 4;
+// var popularNumber = 2 + 2; 
+// alert(popularNumber)
+// var num = 10; 
+// var anotherNum = 1; 
+// var popularNumber = num + anotherNum; 
+// alert(popularNumber);
+// var whatsLeftOver = 10 % 3;
+// alert(whatsLeftOver);
+
+                                       // chap # 6 Math expressions: Unfamiliar operators 
+// num++; incriment
+// num--; decrement 
+// var num = 1;
+// var newNum = num++;
+// console.log(newNum); 
+// var num = 1;
+// var newNum = num--;
+// console.log(newNum); 
+
+                                       // chap # 7 Math expressions: Eliminating ambiguity 
+// var totalCost = 1 + 3 * 4;
+// alert(totalCost);
+// var resultOfComputation = (2 * 4) * 4 + 2;
+// alert(resultOfComputation); 
+
+                                      // chap # 8 Concatenating text strings
+// var message = "Thanks for your input!";  
+// alert(message);
+// var userName = "Adil";
+// var message = "Thanks, "; 
+// var banger = "!"; 
+// alert(message + userName + banger);
+// alert("Thanks, " + userName + "!");
+// alert("2" + "2"); 
+
+                                      // chap # 9 Promt
+// var spec = prompt("Your species?", "human"); 
+// var numberOfCats = prompt("How many cats?"); 
+// var tooManyCats = numberOfCats + 1;
+// alert(tooManyCats); 
+// var name = prompt("Enter your name");
+// alert(name);
+
+                                      // chap # 10 if statment
+// var x  = prompt("Where does the Pope live?"); 
+//  if (x === "Vatican") {
+//         alert("Correct!");
+// } 
+// var correctAnswer = "Vatican"; 
+// if (x === correctAnswer) { 
+//      score++;
+//      userIQ = "genius";
+//      alert("Correct!");
+// } 
+
+                                      // chap # 11 Compersion operators
+// if (yourTicketNumber !== 487208) {    
+//     alert("Better luck next time.");
+// } 
+// if (fullName === "Mark" + " " + "Myers") {
+//      if (fullName === firstName + " " + "Myers") {
+//          if (fullName === firstName + " " + "Myers") {
+//              if (fullName === "firstName + " " + lastName) { 
+//                 if (totalCost === 81.50 + 135) { 
+//                     if (totalCost === materialsCost + 135) { 
+//                         if (totalCost === materialsCost + laborCost) { 
+//                             if (x + y === a - b) {
+
+                                      // chap # 12 if...else and else if statements 
+// var x = prompt("Where does the Pope live?"); 
+// if (x === "Vatican") {
+//     alert("Correct!"); 
+// }
+
+// if (x !== "Vatican") {
+//     alert("Wrong answer");
+// } 
+// var x = prompt("Where does the Pope live?");
+// if (x === "Vatican") {
+//         alert("Correct!"); 
+//     }
+// else{ alert("Wrong answer"); 
+// }
+// var x = prompt("Where does the Pope live?");
+// var correctAnswer = "Vatican";
+// if (x === correctAnswer) {
+//   alert("Correct!");
+// }
+// else if (x === "Rome") {
+//     alert("Incorrect but close"); 
+// }
+// else{ alert("Wrong answer"); 
+// }
+
+                                      // chap # 13 Testing sets of conditions 
+// var a = prompt("enter here");
+// if (weight > 300 && time < 6 && age > 17 && gender === "male") {
+//     alert("Come to our tryout!");
+// }
+// else {
+//     alert("Come to our cookout!");
+// } 
+
+                                      // chap # 14 if statements nested 
+// if  ((x === y || a === b) && c === d) {
+//     g = h;
+// }
+// else { 
+//     e = f;
+// } 
+                                      // chap # 15 Arrays
+// var city0 = "Atlanta";
+// var city1 = "Baltimore"; 
+// var city2 = "Chicago";
+// var city3 = "Denver"; 
+// var city4 = "Los Angeles"; 
+// var city5 = "Seattle";   
+// alert("Welcome to " + city3);
+// var cities = ["Atlanta", "Baltimore", "Chicago", "Denver", "Los Angeles", "Seattle"]; 
+// alert("Welcome to " + cities[3]);
+
+
+                                      // chap # 
+                                      // chap # 
+                                      // chap # 
+                                      // chap # 
+                                      // chap # 
